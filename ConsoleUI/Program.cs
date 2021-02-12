f4ab1652c4c78f3cc100a932ca0ffe2a64e5480b
Gülden Sönmez <gulden.arslanoglu@boun.edu.tr>
2021-02-12 23:00:41 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ProductTest();
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetByUnitPrice(50, 150))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
